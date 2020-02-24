1a901bea984f3d995f73423f465c547f067ed4e4
Kavithvajen <kavithvajen@gmail.com>
2020-02-24 06:53:39 +0000

    Can now input multiple datasets. More tags added.

--- tool.py
+++ tool.py
@@ -1,8 +1,6 @@
 import rdflib
-import requests
-import sys
-import json
-import logging
+import requests, json
+import sys, os, logging
 
 def checkVocab(vocab):
     URL = "https://lov.linkeddata.es/dataset/lov/api/v2/vocabulary/info?"
@@ -22,21 +20,48 @@ def checkVocab(vocab):
     for tag in data["tags"]:
         if tag == "Geography":
             print("\nNOTE: This dataset probably contains location related data as it uses the {} namespace!".format(data["prefix"]))
+        elif tag == "Society":
+            print("\nNOTE: This dataset probably contains society related data as it uses the {} namespace!".format(data["prefix"]))
+        elif tag == "People":
+            print("\nNOTE: This dataset probably contains people related data as it uses the {} namespace!".format(data["prefix"]))
+        elif tag == "Health":
+            print("\nNOTE: This dataset probably contains health related data as it uses the {} namespace!".format(data["prefix"]))
+        elif tag == "Biology":
+            print("\nNOTE: This dataset probably contains biology related data as it uses the {} namespace!".format(data["prefix"]))
+        elif tag == "Government":
+            print("\nNOTE: This dataset probably contains government related data as it uses the {} namespace!".format(data["prefix"]))
+        elif tag == "Environment":
+            print("\nNOTE: This dataset probably contains environment related data as it uses the {} namespace!".format(data["prefix"]))
+        else:
+            pass
+
+def loadDatasets():
+    os.chdir("Input")
+    datasetList = os.listdir()
+
+    graphList = []
+
+    for dataset in datasetList:
+        graphList.append(rdflib.Graph())
+        graphList[-1].parse(dataset, format = rdflib.util.guess_format("/"+dataset))
+        print("\nSuccessfully loaded the \"{}\" dataset. \nNow checking the vocabulary used in the dataset to find potential issues.".format(dataset))
+        for vocab in graphList[-1].namespace_manager.namespaces():
+            checkVocab(vocab[0])
 
 def main():
 
     #The following line is to suppress a common warning message by the rdflib package.
     logging.getLogger("rdflib").setLevel(logging.ERROR)
 
-    print("\nStarted the tool successfully. Now going to load the dataset.")
-
-    g = rdflib.Graph()
-    g.parse("COLINDA.rdf", format = rdflib.util.guess_format("/COLINDA.rdf"))
-
-    print("\nSuccessfully loaded the dataset. Now checking the vocabulary used in the dataset to find potential issues.")
-
-    for vocab in g.namespace_manager.namespaces():
-        checkVocab(vocab[0])
+    option = input("\nStarted the tool successfully. Are all the input datasets in the \"Input\" folder? [Y/N]: ")
+    if option == "Y" or option == "y":
+        loadDatasets()
+    elif option == "N" or option == "n":
+        print("Okay! Make sure all the input datasets are in the \"Input\" folder and then start the tool.")
+        sys.exit()
+    else:
+        print("Wrong option. Aborting program!")
+        sys.exit()
 
     print("\nTool finished running.")
 
