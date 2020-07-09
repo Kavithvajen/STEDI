515d3292e4ab8ab7beee1a8c67f76ebdf8babe0a
Kavith <kamarajk@tcd.ie>
2020-07-09 22:38:22 +0100

    Able to add named individuals to the ethics ontology

--- ontology-test.py
+++ ontology-test.py
@@ -1,3 +1,21 @@
 import rdflib
+from rdflib.namespace import RDF, OWL
+import os
 
-print ("Test")
+os.chdir("Ontology")
+#ontology = "/Users/kavith/Kavith/University/TCD/Dissertation/Development/Dissertation/Ontology/EthicsOntology.owl"
+
+ont_graph = rdflib.Graph()
+ont_graph.parse("EthicsOntology.owl", format = rdflib.util.guess_format('/EthicsOntology.owl'))
+print("Parsed")
+
+# for s, p, o in ont_graph:
+#     print(f"\nSUBJECT: {s}\nPREDICATE: {p}\nOBJECT: {o}\n")
+
+one = rdflib.URIRef("https://www.scss.tcd.ie/~kamarajk/EthicsOntology#one")
+
+ont_graph.add((one, RDF.type, OWL.NamedIndividual))
+
+for p,o in ont_graph.predicate_objects(one):
+    print("IN")
+    print(f"\nSUBJECT: {one}\nPREDICATE: {p}\nOBJECT: {o}\n")
