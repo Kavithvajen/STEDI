ed99c019364c15d6634494c4bb15e338509d65c9
Kavith <kamarajk@tcd.ie>
2020-07-10 16:27:13 +0100

    Able to push to ethics ontology

--- ontology-test.py
+++ ontology-test.py
@@ -2,23 +2,24 @@ import rdflib
 from rdflib.namespace import RDF, OWL
 import os
 
-os.chdir("Ontology")
+#os.chdir("Ontology")
 #ontology = "/Users/kavith/Kavith/University/TCD/Dissertation/Development/Dissertation/Ontology/EthicsOntology.owl"
 
 ont_graph = rdflib.Graph()
-ont_graph.parse("EthicsOntology.owl", format = rdflib.util.guess_format('/EthicsOntology.owl'))
+ont_graph.parse("Ontology/EthicsOntology.owl", format = rdflib.util.guess_format('/Ontology/EthicsOntology.owl'))
 print("Parsed")
 
 # for s, p, o in ont_graph:
 #     print(f"\nSUBJECT: {s}\nPREDICATE: {p}\nOBJECT: {o}\n")
 
-one = rdflib.URIRef("https://www.scss.tcd.ie/~kamarajk/EthicsOntology#one")
+#one = rdflib.URIRef("https://www.scss.tcd.ie/~kamarajk/EthicsOntology#one")
+n = rdflib.Namespace("https://www.scss.tcd.ie/~kamarajk/EthicsOntology#")
 
-ont_graph.add((one, RDF.type, OWL.NamedIndividual))
+ont_graph.add((n.one, RDF.type, OWL.NamedIndividual))
 
-for p,o in ont_graph.predicate_objects(one):
+for p,o in ont_graph.predicate_objects(n.one):
     print("IN")
-    print(f"\nSUBJECT: {one}\nPREDICATE: {p}\nOBJECT: {o}\n")
+    print(f"\nSUBJECT: {n.one}\nPREDICATE: {p}\nOBJECT: {o}\n")
 
 print("Writing to a file")
 
--- tool.py
+++ tool.py
@@ -41,15 +41,15 @@ def predicate_issues(graph):
             'politics', 'race', 'religion', 'resident', 'salary', 'search', 'size', 'skin', 'tattoos','tracking',
             'weight']
 
-    ethics_ontology_issues = {"age": False, "behaviour": False, "body": False, "child": False, "contact": False,
-            "criminal": False, "data_controller": False, "dob": False, "doctor": False, "ethnic": False,
-            "files": False, "health": False, "income": False, "loan": False, "location": False, "name": False,
-            "nda": False, "physical": False, "politics": False, "religion": False, "too_much_data": False,
-            "tracking": False, "valid_processing": False}
+    ethics_ontology_dictionary = {"hasAge": False, "hasBehaviourData": False, "hasBodyStatistics": False, "isChild": False, "hasContactInformation": False,
+            "hasCriminalActivity": False, "hasDataControllerName": False, "hasDoctorConsultationsData": False, "hasEthnicityData": False,
+            "hasFilesWithPIIAttached": False, "hasHealthData": False, "hasIncomeData": False, "hasLoanRecords": False, "hasLocationData": False, "hasName": False,
+            "hasSignedNDA": False, "hasPhysicalCharacteristics": False, "hasPoliticalOpinions": False, "hasReligion": False, "hasTooManyDataPoints": False,
+            "hasUserTrackingData": False, "isValidForProcessing": False}
 
     common_words_to_ignore = ["syntax", "same", "as", "spatial"]
 
-    age_words = ["age", "birthday"]
+    age_words = ["age", "birthday", "dob"]
     behaviour_words = ["behaviour", "personality", "myers", "opinion"]
     body_words = ["body", "height", "weight", "size"]
     contact_words = ["contact", "phone", "email"]
@@ -84,65 +84,123 @@ def predicate_issues(graph):
                     if token.has_vector and token.similarity(nlp(issue)) > 0.5:
                             print(f"Contains {issue} related data! -> {token}")
                             if issue in age_words:
-                                ethics_ontology_issues["age"] = True
-                                ethics_ontology_issues["dob"] = True
+                                ethics_ontology_dictionary["hasAge"] = True
                             elif issue in behaviour_words:
-                                ethics_ontology_issues["behaviour"] = True
+                                ethics_ontology_dictionary["hasBehaviourData"] = True
                             elif issue in body_words:
-                                ethics_ontology_issues["body"] = True
+                                ethics_ontology_dictionary["hasBodyStatistics"] = True
                             elif issue in contact_words:
-                                ethics_ontology_issues["contact"] = True
+                                ethics_ontology_dictionary["hasContactInformation"] = True
                             elif issue in criminal_words:
-                                ethics_ontology_issues["criminal"] = True
+                                ethics_ontology_dictionary["hasCriminalActivity"] = True
                             elif issue in doctor_words:
-                                ethics_ontology_issues["doctor"] = True
+                                ethics_ontology_dictionary["hasDoctorConsultationsData"] = True
                             elif issue in ethnic_words:
-                                ethics_ontology_issues["ethnic"] = True
+                                ethics_ontology_dictionary["hasEthnicityData"] = True
                             elif issue in health_words:
-                                ethics_ontology_issues["health"] = True
+                                ethics_ontology_dictionary["hasHealthData"] = True
                             elif issue in income_words:
-                                ethics_ontology_issues["income"] = True
+                                ethics_ontology_dictionary["hasIncomeData"] = True
                             elif issue in loan_words:
-                                ethics_ontology_issues["loan"] = True
+                                ethics_ontology_dictionary["hasLoanRecords"] = True
                             elif issue in location_words:
-                                ethics_ontology_issues["location"] = True
+                                ethics_ontology_dictionary["hasLocationData"] = True
                             elif issue in name_words:
-                                ethics_ontology_issues["name"] = True
+                                ethics_ontology_dictionary["hasName"] = True
                             elif issue in physical_words:
-                                ethics_ontology_issues["physical"] = True
+                                ethics_ontology_dictionary["hasPhysicalCharacteristics"] = True
                             elif issue in politics_words:
-                                ethics_ontology_issues["politics"] = True
+                                ethics_ontology_dictionary["hasPoliticalOpinions"] = True
                             elif issue in religion_words:
-                                ethics_ontology_issues["religion"] = True
+                                ethics_ontology_dictionary["hasReligion"] = True
                             elif issue in tracking_words:
-                                ethics_ontology_issues["tracking"] = True
+                                ethics_ontology_dictionary["hasUserTrackingData"] = True
                             else:
                                 print("We may have a problem!")
 
-    print(f"\nEthics Ontology Issues Dictionary: \n {ethics_ontology_issues}\n")
+    return ethics_ontology_dictionary
 
-def fill_ethics_ontology():
-    pass
+def fill_ethics_ontology(dataset_name, ethics_ontology, ethics_ontology_dictionary):
+    #Ethics Ontology Namespace
+    EONS = rdflib.Namespace("https://www.scss.tcd.ie/~kamarajk/EthicsOntology#")
+
+    #Cleaning up dataset_name so the individuals of the ethics ontology follow a consistent naming convention.
+    dataset_name = dataset_name.split('.')[0]
+    dataset_name = re.sub("[^A-Za-z0-9 ]+", " ", dataset_name)
+    dataset_name = re.sub("[ ]+", " ", dataset_name)
+    dataset_name = dataset_name.replace(" ", "_")
+
+    #Creates a named individual with the name of the dataset
+    ethics_ontology.add((EONS[dataset_name], RDF.type, OWL.NamedIndividual))
+
+    for key, value in ethics_ontology_dictionary.items():
+        #if value == True:
+        ethics_ontology.add((EONS[dataset_name], EONS[key], rdflib.term.Literal(value)))
+
+        # if key == "age":
+        #     ethics_ontology.add((EONS[dataset_name], EONS.hasAge, rdflib.term.Literal(value)))
+        # elif key == "behaviour":
+        #     ethics_ontology.add((EONS[dataset_name], EONS.hasBehaviourData, rdflib.term.Literal(value)))
+        # elif key == "body":
+        #     ethics_ontology.add((EONS[dataset_name], EONS.hasBodyStatistics, rdflib.term.Literal(value)))
+        # elif key == "contact":
+        #     ethics_ontology.add((EONS[dataset_name], EONS.hasContactInformation, rdflib.term.Literal(value)))
+        # elif key == "criminal":
+        #     ethics_ontology.add((EONS[dataset_name], EONS.hasCriminalActivity, rdflib.term.Literal(value)))
+        # elif key == "doctor":
+        #     ethics_ontology.add((EONS[dataset_name], EONS.hasDoctorConsultationsData, rdflib.term.Literal(value)))
+        # elif key == "ethnic":
+        #     ethics_ontology.add((EONS[dataset_name], EONS.hasEthnicityData, rdflib.term.Literal(value)))
+        # elif key == "health":
+        #     ethics_ontology.add((EONS[dataset_name], EONS.hasHealthData, rdflib.term.Literal(value)))
+        # elif key == "income":
+        #     ethics_ontology.add((EONS[dataset_name], EONS.hasIncomeData, rdflib.term.Literal(value)))
+        # elif key == "loan":
+        #     ethics_ontology.add((EONS[dataset_name], EONS.hasLoanRecords, rdflib.term.Literal(value)))
+        # elif key == "location":
+        #     ethics_ontology.add((EONS[dataset_name], EONS.hasLocationData, rdflib.term.Literal(value)))
+        # elif key == "name":
+        #     ethics_ontology.add((EONS[dataset_name], EONS.hasName, rdflib.term.Literal(value)))
+        # elif key == "physical":
+        #     ethics_ontology.add((EONS[dataset_name], EONS.hasPhysicalCharacteristics, rdflib.term.Literal(value)))
+        # elif key == "politics":
+        #     ethics_ontology.add((EONS[dataset_name], EONS.hasBehaviourData, rdflib.term.Literal(value)))
+        # elif key == "religion":
+        #     ethics_ontology.add((EONS[dataset_name], EONS.hasBehaviourData, rdflib.term.Literal(value)))
+        # elif key == "tracking":
+        #     ethics_ontology.add((EONS[dataset_name], EONS.hasBehaviourData, rdflib.term.Literal(value)))
+        # else:
+        #     pass
+    return ethics_ontology
 
 def integration_issues():
     pass
 
 def start_execution():
-    os.chdir("Input")
-    dataset_list = [f for f in os.listdir() if not f.startswith(".")]
+    # Importing the ethics ontology
+    ethics_ontology = rdflib.Graph()
+    ethics_ontology.parse("Ontology/EthicsOntology.owl", format = rdflib.util.guess_format('/Ontology/EthicsOntology.owl'))
+
+    # Importing the input datasets
+    dataset_list = [f for f in os.listdir("Input") if not f.startswith(".")]
     graph_list = []
     for dataset in dataset_list:
         graph_list.append(rdflib.Graph())
-        graph_list[-1].parse(dataset, format = rdflib.util.guess_format(f"/{dataset}"))
+        graph_list[-1].parse(f"Input/{dataset}", format = rdflib.util.guess_format(f"/Input/{dataset}"))
         graph_list[-1].serialize(format="xml")
         print("\nSuccessfully loaded the \"{}\" dataset. \nNow checking the vocabulary used in the dataset to find potential issues.".format(dataset))
-        for vocab in graph_list[-1].namespace_manager.namespaces():
-            check_vocab(vocab[0])
+        # for vocab in graph_list[-1].namespace_manager.namespaces():
+        #     check_vocab(vocab[0])
 
         print("\n\nCHECKING FOR POSSIBLE ETHICAL ISSUES IN THE PREDICATES!\n\n")
-        predicate_issues(graph_list[-1])
+        ethics_ontology_dictionary = predicate_issues(graph_list[-1])
 
-    integration_issues()
+        ethics_ontology = fill_ethics_ontology(dataset, ethics_ontology, ethics_ontology_dictionary)
+
+    ethics_ontology.serialize(destination='Output/Updated_Ethics_Ontology.owl', format='xml')
+    print("Output - Updated Ethics Ontology created")
+
+    # integration_issues()
 
 def main():
     #The following line is to suppress a common warning message by the rdflib package.
@@ -152,7 +210,7 @@ def main():
     if option == "Y" or option == "y":
         start_execution()
     elif option == "N" or option == "n":
-        print("Okay! Make sure all the input datasets are in the \"Input\" folder and then start the tool.")
+        print("\nOkay! Make sure all the input datasets are in the \"Input\" folder and then start the tool.")
         sys.exit()
     else:
         print("Wrong option. Aborting program!")
