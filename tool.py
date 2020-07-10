1a1766f6574d0b3bb05fcc54875d83ba301558c4
Kavith <kamarajk@tcd.ie>
2020-07-10 20:51:28 +0100

    Refactored code

--- tool.py
+++ tool.py
@@ -31,7 +31,6 @@ def check_vocab(vocab):
                 print(f"\nNOTE: This dataset probably contains {namespace} related data as it uses the {data['prefix']} namespace!")
 
 def predicate_issues(graph):
-    #os.chdir("Input")
     nlp = spacy.load("en_core_web_lg")
 
     issue_tokens = ['accent', 'address', 'advertisement', 'age', 'behaviour', 'birthday', 'body', 'browser',
@@ -41,30 +40,66 @@ def predicate_issues(graph):
             'politics', 'race', 'religion', 'resident', 'salary', 'search', 'size', 'skin', 'tattoos','tracking',
             'weight']
 
-    ethics_ontology_dictionary = {"hasAge": False, "hasBehaviourData": False, "hasBodyStatistics": False, "isChild": False, "hasContactInformation": False,
-            "hasCriminalActivity": False, "hasDataControllerName": False, "hasDoctorConsultationsData": False, "hasEthnicityData": False,
-            "hasFilesWithPIIAttached": False, "hasHealthData": False, "hasIncomeData": False, "hasLoanRecords": False, "hasLocationData": False, "hasName": False,
-            "hasSignedNDA": False, "hasPhysicalCharacteristics": False, "hasPoliticalOpinions": False, "hasReligion": False, "hasTooManyDataPoints": False,
-            "hasUserTrackingData": False, "isValidForProcessing": False}
+    ethics_ontology_dictionary = {"hasAge": False, "hasBehaviourData": False, "hasBodyStatistics": False,
+            "isChild": False, "hasContactInformation": False, "hasCriminalActivity": False,
+            "hasDataControllerName": False, "hasDoctorConsultationsData": False, "hasEthnicityData": False,
+            "hasFilesWithPIIAttached": False, "hasHealthData": False, "hasIncomeData": False,
+            "hasLoanRecords": False, "hasLocationData": False, "hasName": False, "hasSignedNDA": False,
+            "hasPhysicalCharacteristics": False, "hasPoliticalOpinions": False, "hasReligion": False,
+            "hasTooManyDataPoints": False, "hasUserTrackingData": False, "isValidForProcessing": False}
 
     common_words_to_ignore = ["syntax", "same", "as", "spatial"]
 
-    age_words = ["age", "birthday", "dob"]
-    behaviour_words = ["behaviour", "personality", "myers", "opinion"]
-    body_words = ["body", "height", "weight", "size"]
-    contact_words = ["contact", "phone", "email"]
-    criminal_words = ["criminal", "jail"]
-    doctor_words = ["doctor", "consult"]
-    ethnic_words = ["language", "race", "community", "accent", "dialect", "immigrant", "religion"]
-    health_words = ["health", "medical"]
-    income_words = ["income", "salary"]
-    loan_words = ["loan"]
-    location_words = ["address", "city", "location", "resident"]
-    name_words = ["name"]
-    physical_words = ["gender", "disability", "colour", "skin", "hair", "tattoos", "piercings"]
-    politics_words = ["politics"]
-    religion_words = ["religion"]
-    tracking_words = ["advertisement", "history", "browser", "search", "tracking"]
+    # Cheekily tried to fit in the mapping of the word lists to the key names of the ethics ontology.
+    word_lists = {
+        "age_words" : (("age", "birthday", "dob"),
+                        "hasAge"),
+
+        "behaviour_words" : (("behaviour", "personality", "myers", "opinion"),
+                        "hasBehaviourData"),
+
+        "body_words" : (("body", "height", "weight", "size"),
+                        "hasBodyStatistics"),
+
+        "contact_words" : (("contact", "phone", "email"),
+                        "hasContactInformation"),
+
+        "criminal_words" : (("criminal", "jail"),
+                        "hasCriminalActivity"),
+
+        "doctor_words" : (("doctor", "consult"),
+                        "hasDoctorConsultationsData"),
+
+        "ethnic_words" : (("language", "race", "community", "accent", "dialect", "immigrant", "religion"),
+                        "hasEthnicityData"),
+
+        "health_words" : (("health", "medical"),
+                        "hasHealthData"),
+
+        "income_words" : (("income", "salary"),
+                        "hasIncomeData"),
+
+        "loan_words" : (("loan"),
+                        "hasLoanRecords"),
+
+        "location_words" : (("address", "city", "location", "resident"),
+                        "hasLocationData"),
+
+        "name_words" : (("name"),
+                        "hasName"),
+
+        "physical_words" : (("gender", "disability", "colour", "skin", "hair", "tattoos", "piercings"),
+                        "hasPhysicalCharacteristics"),
+
+        "politics_words" : (("politics"),
+                        "hasPoliticalOpinions"),
+
+        "religion_words" : (("religion"),
+                        "hasReligion"),
+
+        "tracking_words" : (("advertisement", "history", "browser", "search", "tracking"),
+                        "hasUserTrackingData")
+    }
 
     for p in graph.predicates():
         predicate_parts = p.split("/")
@@ -83,40 +118,11 @@ def predicate_issues(graph):
                     # To avoid checking similarity for empty vectors.
                     if token.has_vector and token.similarity(nlp(issue)) > 0.5:
                             print(f"Contains {issue} related data! -> {token}")
-                            if issue in age_words:
-                                ethics_ontology_dictionary["hasAge"] = True
-                            elif issue in behaviour_words:
-                                ethics_ontology_dictionary["hasBehaviourData"] = True
-                            elif issue in body_words:
-                                ethics_ontology_dictionary["hasBodyStatistics"] = True
-                            elif issue in contact_words:
-                                ethics_ontology_dictionary["hasContactInformation"] = True
-                            elif issue in criminal_words:
-                                ethics_ontology_dictionary["hasCriminalActivity"] = True
-                            elif issue in doctor_words:
-                                ethics_ontology_dictionary["hasDoctorConsultationsData"] = True
-                            elif issue in ethnic_words:
-                                ethics_ontology_dictionary["hasEthnicityData"] = True
-                            elif issue in health_words:
-                                ethics_ontology_dictionary["hasHealthData"] = True
-                            elif issue in income_words:
-                                ethics_ontology_dictionary["hasIncomeData"] = True
-                            elif issue in loan_words:
-                                ethics_ontology_dictionary["hasLoanRecords"] = True
-                            elif issue in location_words:
-                                ethics_ontology_dictionary["hasLocationData"] = True
-                            elif issue in name_words:
-                                ethics_ontology_dictionary["hasName"] = True
-                            elif issue in physical_words:
-                                ethics_ontology_dictionary["hasPhysicalCharacteristics"] = True
-                            elif issue in politics_words:
-                                ethics_ontology_dictionary["hasPoliticalOpinions"] = True
-                            elif issue in religion_words:
-                                ethics_ontology_dictionary["hasReligion"] = True
-                            elif issue in tracking_words:
-                                ethics_ontology_dictionary["hasUserTrackingData"] = True
-                            else:
-                                print("We may have a problem!")
+                            # A complex shorthand but saves a lot of if-else conditions.
+                            # It basically searches for the issue in every tuple in the word_lists dictionary.
+                            # It also assigns the appropriate key names of the ethics ontology dictionary.
+                            data_property = [word_tuple[1] for word_tuple in word_lists.values() if issue in word_tuple[0]][0]
+                            ethics_ontology_dictionary[data_property] = True
 
     return ethics_ontology_dictionary
 
@@ -134,43 +140,8 @@ def fill_ethics_ontology(dataset_name, ethics_ontology, ethics_ontology_dictiona
     ethics_ontology.add((EONS[dataset_name], RDF.type, OWL.NamedIndividual))
 
     for key, value in ethics_ontology_dictionary.items():
-        #if value == True:
         ethics_ontology.add((EONS[dataset_name], EONS[key], rdflib.term.Literal(value)))
 
-        # if key == "age":
-        #     ethics_ontology.add((EONS[dataset_name], EONS.hasAge, rdflib.term.Literal(value)))
-        # elif key == "behaviour":
-        #     ethics_ontology.add((EONS[dataset_name], EONS.hasBehaviourData, rdflib.term.Literal(value)))
-        # elif key == "body":
-        #     ethics_ontology.add((EONS[dataset_name], EONS.hasBodyStatistics, rdflib.term.Literal(value)))
-        # elif key == "contact":
-        #     ethics_ontology.add((EONS[dataset_name], EONS.hasContactInformation, rdflib.term.Literal(value)))
-        # elif key == "criminal":
-        #     ethics_ontology.add((EONS[dataset_name], EONS.hasCriminalActivity, rdflib.term.Literal(value)))
-        # elif key == "doctor":
-        #     ethics_ontology.add((EONS[dataset_name], EONS.hasDoctorConsultationsData, rdflib.term.Literal(value)))
-        # elif key == "ethnic":
-        #     ethics_ontology.add((EONS[dataset_name], EONS.hasEthnicityData, rdflib.term.Literal(value)))
-        # elif key == "health":
-        #     ethics_ontology.add((EONS[dataset_name], EONS.hasHealthData, rdflib.term.Literal(value)))
-        # elif key == "income":
-        #     ethics_ontology.add((EONS[dataset_name], EONS.hasIncomeData, rdflib.term.Literal(value)))
-        # elif key == "loan":
-        #     ethics_ontology.add((EONS[dataset_name], EONS.hasLoanRecords, rdflib.term.Literal(value)))
-        # elif key == "location":
-        #     ethics_ontology.add((EONS[dataset_name], EONS.hasLocationData, rdflib.term.Literal(value)))
-        # elif key == "name":
-        #     ethics_ontology.add((EONS[dataset_name], EONS.hasName, rdflib.term.Literal(value)))
-        # elif key == "physical":
-        #     ethics_ontology.add((EONS[dataset_name], EONS.hasPhysicalCharacteristics, rdflib.term.Literal(value)))
-        # elif key == "politics":
-        #     ethics_ontology.add((EONS[dataset_name], EONS.hasBehaviourData, rdflib.term.Literal(value)))
-        # elif key == "religion":
-        #     ethics_ontology.add((EONS[dataset_name], EONS.hasBehaviourData, rdflib.term.Literal(value)))
-        # elif key == "tracking":
-        #     ethics_ontology.add((EONS[dataset_name], EONS.hasBehaviourData, rdflib.term.Literal(value)))
-        # else:
-        #     pass
     return ethics_ontology
 
 def integration_issues():
@@ -189,8 +160,8 @@ def start_execution():
         graph_list[-1].parse(f"Input/{dataset}", format = rdflib.util.guess_format(f"/Input/{dataset}"))
         graph_list[-1].serialize(format="xml")
         print("\nSuccessfully loaded the \"{}\" dataset. \nNow checking the vocabulary used in the dataset to find potential issues.".format(dataset))
-        # for vocab in graph_list[-1].namespace_manager.namespaces():
-        #     check_vocab(vocab[0])
+        for vocab in graph_list[-1].namespace_manager.namespaces():
+            check_vocab(vocab[0])
 
         print("\n\nCHECKING FOR POSSIBLE ETHICAL ISSUES IN THE PREDICATES!\n\n")
         ethics_ontology_dictionary = predicate_issues(graph_list[-1])
