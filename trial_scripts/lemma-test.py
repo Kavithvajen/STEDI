a3ec405ec54aa7a61fd8c94d7d3afed8e18d6e84
Kavith <kamarajk@tcd.ie>
2020-07-23 21:18:02 +0100

    Removed trial scripts

--- trial_scripts/lemma-test.py
+++
@@ -1,9 +0,0 @@
-import spacy
-
-nlp = spacy.load("en_core_web_lg")
-
-n = nlp("finance")
-
-print(f"n.has_vector: {n.has_vector}")
-print(f'n.similarity(nlp("financial")): {n.similarity(nlp("financial"))}')
-#if token.has_vector and token.similarity(nlp(issue)) > 0.5:
